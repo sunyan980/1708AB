c682634b3a84aa69061a9737cbd2bd5f753a4ec0
sun <sun@LAPTOP-MKCGS0B2>
2020-03-20 18:57:59 +0800

    第二次

---
+++ Day0320/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day0320
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sunyan");
+
+            Console.WriteLine("sunyan1");
+            Console.WriteLine("sunyan11");
+        }
+    }
+}
